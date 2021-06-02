9008c4dfae606371509d1c08039e1017e1e5319a
Jiri Novotny <jiri.novotny@ericsson.com>
2021-06-02 12:09:27 +0300

    gitignore and prereqs update

--- scripts/prerequirements.sh
+++ scripts/prerequirements.sh
@@ -5,7 +5,9 @@
 # 		- then update?
 # else
 #		install rust
-
+sudo apt-get install build-essential -y
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
 
 #Create target directory in EC2
 DIR="/home/ubuntu/battlesnake"
