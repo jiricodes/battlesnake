06e873b544a16c6a6fe8591280c32dd714b94b6b
Jiri Novotny <jiri.novotny@ericsson.com>
2021-06-02 13:55:52 +0300

    test deploy

--- scripts/prerequirements.sh
+++ scripts/prerequirements.sh
@@ -9,6 +9,7 @@ sudo apt-get install build-essential -y
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
+
 #Create target directory in EC2
 DIR="/home/ubuntu/battlesnake"
 if [ -d "$DIR" ]; then
